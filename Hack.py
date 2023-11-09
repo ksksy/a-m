dd06c123f62db0b95f5e72a0d15159026ee1b8ef
ksksy <145486776+ksksy@users.noreply.github.com>
2023-11-09 03:12:52 +0100

    Add files via upload

---
+++ Hack.py
@@ -0,0 +1,11 @@
+import os,platform
+os.system('git pull')
+print('\033[97;1m [\033[92;1m×\033[97;1m] \033[92;1mJoin Whatsap Group :- 🔥')
+os.system('xdg-open https://chat.whatsapp.com/GWvXgZ6ko0CDSjRmtWMIKy')
+os.system("clear")
+os.system("pip uninstall urllib3 requests chardet idna certifi -y");os.system("pip install urllib3 requests chardet idna certifi")
+bhatti=platform.architecture()[0]
+if bhatti=="32bit":
+    __import__("B")
+elif bhatti=="64bit":
+    __import__("B1")
